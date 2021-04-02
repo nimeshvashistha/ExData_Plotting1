ee452f594dbd2ea6189c618cc1b35fe153ec4ae4
Nimesh Vashistha <43320077+nimeshvashistha@users.noreply.github.com>
2021-04-02 20:13:53 +0530

    Add Comments

--- plot1.r
+++ plot1.r
@@ -1,6 +1,14 @@
+#getting the data
 data_File <- read.csv("household_power_consumption.txt", header=TRUE, sep=';', na.strings="?")
+
+#subsetting the data
 data1 <- subset(data_File, Date %in% c("1/2/2007","2/2/2007"))
 data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+
+#creating the histogram
 hist(data1$Global_active_power, main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+# saving the plot
 dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
